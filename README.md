06deeec9c00f0ed4ae44b63ba690751e10c14315
AgungPrawoto13 <agungprawoto997@gmail.com>
2024-02-09 09:55:30 +0700

    new update file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-web scraping
+pada assigntment kali ini saya menggunakan link api dari 
+https://mixedanalytics.com/blog/list-actually-free-open-no-auth-needed-apis/
+
+kemudian saya memilih link api public holiday yang ada di Amerika Serikat
+https://date.nager.at/api/v2/publicholidays/2020/US
+
+Tujuan utama dari kegiatan scraping data ini adalah untuk mengumpulkan informasi terkait dengan liburan umum atau public holiday yang ada di Amerika Serikat. Data yang diambil mencakup beberapa aspek kunci seperti:
+
+Tanggal Liburan: Memuat informasi tentang tanggal spesifik di mana liburan tersebut dirayakan.
+Nama Lokasi: Menyajikan nama lokasi atau wilayah di Amerika Serikat di mana liburan tersebut diamati.
+Kode Negara: Menyertakan kode negara yang mengidentifikasi Amerika Serikat sebagai lokasi tempat liburan diobservasi.
+Tipe Liburan: Mengklasifikasikan liburan apakah bersifat global atau terbatas pada wilayah tertentu di Amerika Serikat.
+Tahun Peluncuran: Menunjukkan tahun ketika liburan tersebut pertama kali diakui atau dirayakan.
+
+Dengan mengumpulkan data ini, tujuan utama adalah untuk memahami dan menganalisis pola liburan di Amerika Serikat, mengidentifikasi apakah suatu liburan bersifat global atau terbatas pada beberapa wilayah, dan menyelidiki sejarah atau asal-usul liburan tersebut.
---
+++ assignment_web_scraping.sql
@@ -0,0 +1,14 @@
+INSERT INTO test.assignment_web_scraping (`date`,localName,name,countryCode,fixed,`global`,counties,launchYear,`type`) VALUES
+	 ('2020-01-01','New Year''s Day','New Year''s Day','US',0,1,'None',NULL,'Public'),
+	 ('2020-01-20','Martin Luther King, Jr. Day','Martin Luther King, Jr. Day','US',0,1,'None',NULL,'Public'),
+	 ('2020-02-17','Presidents Day','Washington''s Birthday','US',0,1,'None',NULL,'Public'),
+	 ('2020-04-10','Good Friday','Good Friday','US',0,0,'[''US-CT'', ''US-DE'', ''US-HI'', ''US-IN'', ''US-KY'', ''US-LA'', ''US-NC'', ''US-ND'', ''US-NJ'', ''US-TN'']',NULL,'Public'),
+	 ('2020-04-10','Good Friday','Good Friday','US',0,0,'[''US-TX'']',NULL,'Optional'),
+	 ('2020-05-25','Memorial Day','Memorial Day','US',0,1,'None',NULL,'Public'),
+	 ('2020-07-03','Independence Day','Independence Day','US',0,1,'None',NULL,'Public'),
+	 ('2020-09-07','Labor Day','Labour Day','US',0,1,'None',NULL,'Public'),
+	 ('2020-10-12','Columbus Day','Columbus Day','US',0,0,'[''US-AL'', ''US-AZ'', ''US-CO'', ''US-CT'', ''US-GA'', ''US-ID'', ''US-IL'', ''US-IN'', ''US-IA'', ''US-KS'', ''US-KY'', ''US-LA'', ''US-ME'', ''US-MD'', ''US-MA'', ''US-MS'', ''US-MO'', ''US-MT'', ''US-NE'', ''US-NH'', ''US-NJ'', ''US-NM'', ''US-NY'', ''US-NC'', ''US-OH'', ''US-OK'', ''US-PA'', ''US-RI'', ''US-SC'', ''US-TN'', ''US-UT'', ''US-VA'', ''US-WV'']',NULL,'Public'),
+	 ('2020-11-11','Veterans Day','Veterans Day','US',0,1,'None',NULL,'Public');
+INSERT INTO test.assignment_web_scraping (`date`,localName,name,countryCode,fixed,`global`,counties,launchYear,`type`) VALUES
+	 ('2020-11-26','Thanksgiving Day','Thanksgiving Day','US',0,1,'None',1863.0,'Public'),
+	 ('2020-12-25','Christmas Day','Christmas Day','US',0,1,'None',NULL,'Public');
