a084057e9146f32f0808a1bdefc50fb11f177af4
AgungPrawoto13 <agungprawoto997@gmail.com>
2024-02-09 11:56:17 +0700

    new update data

--- assignment_web_scraping.sql
+++ assignment_web_scraping.sql
@@ -1,14 +1,18 @@
-INSERT INTO test.assignment_web_scraping (`date`,localName,name,countryCode,fixed,`global`,counties,launchYear,`type`) VALUES
-	 ('2020-01-01','New Year''s Day','New Year''s Day','US',0,1,'None',NULL,'Public'),
-	 ('2020-01-20','Martin Luther King, Jr. Day','Martin Luther King, Jr. Day','US',0,1,'None',NULL,'Public'),
-	 ('2020-02-17','Presidents Day','Washington''s Birthday','US',0,1,'None',NULL,'Public'),
-	 ('2020-04-10','Good Friday','Good Friday','US',0,0,'[''US-CT'', ''US-DE'', ''US-HI'', ''US-IN'', ''US-KY'', ''US-LA'', ''US-NC'', ''US-ND'', ''US-NJ'', ''US-TN'']',NULL,'Public'),
-	 ('2020-04-10','Good Friday','Good Friday','US',0,0,'[''US-TX'']',NULL,'Optional'),
-	 ('2020-05-25','Memorial Day','Memorial Day','US',0,1,'None',NULL,'Public'),
-	 ('2020-07-03','Independence Day','Independence Day','US',0,1,'None',NULL,'Public'),
-	 ('2020-09-07','Labor Day','Labour Day','US',0,1,'None',NULL,'Public'),
-	 ('2020-10-12','Columbus Day','Columbus Day','US',0,0,'[''US-AL'', ''US-AZ'', ''US-CO'', ''US-CT'', ''US-GA'', ''US-ID'', ''US-IL'', ''US-IN'', ''US-IA'', ''US-KS'', ''US-KY'', ''US-LA'', ''US-ME'', ''US-MD'', ''US-MA'', ''US-MS'', ''US-MO'', ''US-MT'', ''US-NE'', ''US-NH'', ''US-NJ'', ''US-NM'', ''US-NY'', ''US-NC'', ''US-OH'', ''US-OK'', ''US-PA'', ''US-RI'', ''US-SC'', ''US-TN'', ''US-UT'', ''US-VA'', ''US-WV'']',NULL,'Public'),
-	 ('2020-11-11','Veterans Day','Veterans Day','US',0,1,'None',NULL,'Public');
-INSERT INTO test.assignment_web_scraping (`date`,localName,name,countryCode,fixed,`global`,counties,launchYear,`type`) VALUES
-	 ('2020-11-26','Thanksgiving Day','Thanksgiving Day','US',0,1,'None',1863.0,'Public'),
-	 ('2020-12-25','Christmas Day','Christmas Day','US',0,1,'None',NULL,'Public');
+INSERT INTO test.assignment_web_scraping (pokemon_name,price,img) VALUES
+	 ('Bulbasaur','£63.00','https://scrapeme.live/wp-content/uploads/2018/08/001-350x350.png'),
+	 ('Ivysaur','£87.00','https://scrapeme.live/wp-content/uploads/2018/08/002-350x350.png'),
+	 ('Venusaur','£105.00','https://scrapeme.live/wp-content/uploads/2018/08/003-350x350.png'),
+	 ('Charmander','£48.00','https://scrapeme.live/wp-content/uploads/2018/08/004-350x350.png'),
+	 ('Charmeleon','£165.00','https://scrapeme.live/wp-content/uploads/2018/08/005-350x350.png'),
+	 ('Charizard','£156.00','https://scrapeme.live/wp-content/uploads/2018/08/006-350x350.png'),
+	 ('Squirtle','£130.00','https://scrapeme.live/wp-content/uploads/2018/08/007-350x350.png'),
+	 ('Wartortle','£123.00','https://scrapeme.live/wp-content/uploads/2018/08/008-350x350.png'),
+	 ('Blastoise','£76.00','https://scrapeme.live/wp-content/uploads/2018/08/009-350x350.png'),
+	 ('Caterpie','£73.00','https://scrapeme.live/wp-content/uploads/2018/08/010-350x350.png');
+INSERT INTO test.assignment_web_scraping (pokemon_name,price,img) VALUES
+	 ('Metapod','£148.00','https://scrapeme.live/wp-content/uploads/2018/08/011-350x350.png'),
+	 ('Butterfree','£162.00','https://scrapeme.live/wp-content/uploads/2018/08/012-350x350.png'),
+	 ('Weedle','£25.00','https://scrapeme.live/wp-content/uploads/2018/08/013-350x350.png'),
+	 ('Kakuna','£148.00','https://scrapeme.live/wp-content/uploads/2018/08/014-350x350.png'),
+	 ('Beedrill','£168.00','https://scrapeme.live/wp-content/uploads/2018/08/015-350x350.png'),
+	 ('Pidgey','£159.00','https://scrapeme.live/wp-content/uploads/2018/08/016-350x350.png');
